d6ab826b3df8380fc0f397fbbcbc1edf5c41b084
Fedor Sulaev <fedor.sulaev@outlook.com>
2024-04-14 14:38:28 +0300

    initial

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs: {
+    homeConfigurations = {
+      MacBook-Pro-Fedor = inputs.home-manager.lib.homeManagerConfiguration {
+        pkgs = import inputs.nixpkgs {
+          system = "aarch64-darwin";
+	};
+	modules = [
+	  ./nixpkgs/home-manager/mac-os-personal.nix
+	];
+      };
+    };
+  };
+}
---
+++ nixpkgs/home-manager/mac-os-personal.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, libs, ... }:
+{
+  home.homeDirectory = "/Users/fedorsulaev";
+  home.username = "fedorsulaev";
+  home.stateVersion = "23.11";
+
+  home.packages = with pkgs; [
+    bottom
+    neovim
+    tree-sitter
+    lazygit
+  ];
+
+  # Let Home Manager install and manage itself
+  programs.home-manager.enable = true;
+}
