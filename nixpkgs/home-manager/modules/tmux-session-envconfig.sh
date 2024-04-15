45b3ff560d0bbe6ece4412d1cc73e040c7f64a66
Fedor Sulaev <fedor.sulaev@outlook.com>
2024-04-15 21:09:18 +0300

    feat: New session layout

--- nixpkgs/home-manager/modules/tmux-session-envconfig.sh
+++ nixpkgs/home-manager/modules/tmux-session-envconfig.sh
@@ -1,21 +1,24 @@
-# Set a custom session root path. Default is `$HOME`.
-# Must be called before `initialize_session`.
-#session_root "~/Projects/envconfig"
-
-# Create session with specified name if it does not already exist. If no
-# argument is given, session name will be based on layout file name.
 if initialize_session "envconfig"; then
+  window_root "~/.config"
+  new_window "Config"
+  run_cmd "nvim"
 
-  # Create a new window inline within session layout definition.
-  new_window "misc"
+  window_root "~/Projects/DevEnvSetup"
+  new_window "Setup"
+  run_cmd "nvim"
 
-  # Load a defined window layout.
-  #load_window "example"
+  window_root "~/.config"
+  new_window "ConfigGit"
+  run_cmd "lazygit"
 
-  # Select the default active window on session creation.
-  #select_window 1
+  window_root "~/Projects/DevEnvSetup"
+  new_window "SetupGit"
+  run_cmd "lazygit"
 
+  window_root "~/.config"
+  new_window "Terminal"
+  split_h 50
+  run_cmd "cd ~/Projects/DevEnvSetup"
 fi
 
-# Finalize session creation and switch/attach to it.
 finalize_and_go_to_session
