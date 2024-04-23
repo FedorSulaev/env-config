16a137273578bf0b84ec6a4c3f8646eeed330479
Fedor Sulaev <fedor.sulaev@outlook.com>
2024-04-23 22:44:04 +0300

    feat: Nvim plugins setup - file tree, completion, theme

--- nixpkgs/home-manager/modules/nvim-module/neovim.nix
+++ nixpkgs/home-manager/modules/nvim-module/neovim.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }:
 {
-  programs.neovim = {
+  programs.neovim =
+  let
+    toLua = str: "lua << EOF\n${str}\nEOF\n";
+    toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+  in
+  {
     enable = true;
     viAlias = true;
     vimAlias = true;
@@ -11,7 +16,29 @@
       ${builtins.readFile ./keymaps.lua}
     '';
     plugins = with pkgs.vimPlugins; [
-
+      # File tree
+      {
+        plugin = neo-tree-nvim;
+        config = toLuaFile ./plugins/neo-tree.lua;
+      }
+      # Completion
+      luasnip
+      cmp_luasnip
+      cmp-nvim-lsp
+      cmp-path
+      {
+        plugin = nvim-cmp;
+        config = toLuaFile ./plugins/cmp.lua;
+      }
+      {
+        plugin = nvim-autopairs;
+        config = toLuaFile ./plugins/autopairs.lua;
+      }
+      # Theme
+      {
+        plugin = tokyonight-nvim;
+        config = toLuaFile ./plugins/tokyonight.lua;
+      }
     ];
   };
 }
