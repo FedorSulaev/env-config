aa4af95dcff1316aab15a6fcd7cb1337655afeed
Fedor Sulaev <fedor.sulaev@outlook.com>
2024-04-25 22:48:41 +0300

    feat: Custome github plugin support

--- flake.nix
+++ flake.nix
@@ -5,18 +5,23 @@
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # Nvim plugins from GitHub
+    mason-nvim-dap.url = https://github.com/jay-babu/mason-nvim-dap.nvim;
+    mason-nvim-dap.flake = false;
   };
 
-  outputs = inputs: {
-    homeConfigurations = {
-      MacBook-Pro-Fedor = inputs.home-manager.lib.homeManagerConfiguration {
-        pkgs = import inputs.nixpkgs {
-          system = "aarch64-darwin";
-	      };
-	      modules = [
-	        ./nixpkgs/home-manager/mac-os-personal.nix
-	      ];
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      homeConfigurations.MacBook-Pro-Fedor = inputs.home-manager.lib.homeManagerConfiguration {
+	inherit pkgs;
+	modules = [ ./nixpkgs/home-manager/mac-os-personal.nix ];
+	extraSpecialArgs = { inherit inputs; };
       };
     };
-  };
 }
