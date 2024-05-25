b1ef8d42a38f4d950ab26547ce8a74d690423ef5
Fedor Sulaev <fedor.sulaev@outlook.com>
2024-05-25 20:33:13 +0300

    feat: Starship config

--- nixpkgs/home-manager/modules/zsh.nix
+++ nixpkgs/home-manager/modules/zsh.nix
@@ -9,6 +9,50 @@
   };
   programs.starship = {
     enable = true;
-    settings = { };
+    settings = {
+      add_newline = false;
+      format = "$directory$character";
+      right_format = "$all";
+      palette = "gruvbox_dark";
+      palettes = {
+        gruvbox_dark = {
+          color_fg0 = "#fbf1c7";
+          color_bg1 = "#3c3836";
+          color_bg3 = "#665c54";
+          color_blue = "#458588";
+          color_aqua = "#689d6a";
+          color_green = "#98971a";
+          color_orange = "#d65d0e";
+          color_purple = "#b16286";
+          color_red = "#cc241d";
+          color_yellow = "#d79921";
+        };
+      };
+      os = {
+        disabled = false;
+        symbols = {
+          Windows = "󰍲";
+          Ubuntu = "󰕈";
+          SUSE = "";
+          Raspbian = "󰐿";
+          Mint = "󰣭";
+          Macos = "󰀵";
+          Manjaro = "";
+          Linux = "󰌽";
+          Gentoo = "󰣨";
+          Fedora = "󰣛";
+          Alpine = "";
+          Amazon = "";
+          Android = "";
+          Arch = "󰣇";
+          Artix = "󰣇";
+          CentOS = "";
+          Debian = "󰣚";
+          Redhat = "󱄛";
+          RedHatEnterprise = "󱄛";
+        };
+      };
+
+    };
   };
 }
