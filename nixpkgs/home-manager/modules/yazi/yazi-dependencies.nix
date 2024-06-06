4182f65c3628729986452f45c0837c3a3fdd51aa
Fedor Sulaev <fedor.sulaev@outlook.com>
2024-06-06 21:51:09 +0300

    feat: Yazi dependencies

--- nixpkgs/home-manager/modules/yazi/yazi-dependencies.nix
+++ nixpkgs/home-manager/modules/yazi/yazi-dependencies.nix
@@ -2,9 +2,28 @@
 {
   home.packages = with pkgs; [
     file
+    unar
+    poppler
+    ffmpegthumbnailer
   ];
 
   programs.jq = {
+    enable = true;
+  };
+
+  programs.fd = {
+    enable = true;
+  };
+
+  programs.ripgrep = {
+    enable = true;
+  };
+
+  programs.fzf = {
+    enable = true;
+  };
+
+  programs.zoxide = {
     enable = true;
   };
 }
