785b6f162ca5a720a33d4e748bdd4556912a607d
Fedor Sulaev <fedor.sulaev@outlook.com>
2024-06-28 22:34:20 +0300

    fix: Java test shortcut not working

--- nixpkgs/home-manager/modules/nvim-module/testing-module/vim-test.lua
+++ nixpkgs/home-manager/modules/nvim-module/testing-module/vim-test.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 vim.cmd("let test#strategy = 'vimux'")
+vim.cmd("let test#java#gradletest#executable = 'bb test'")
 -- Vim-test
 vim.keymap.set("n", "<leader>t", ":TestNearest<CR>")
 vim.keymap.set("n", "<leader>T", ":TestFile<CR>")
