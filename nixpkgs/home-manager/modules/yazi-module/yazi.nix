202a2eeff2af4302563e336f51136229350887a4
Fedor Sulaev <fedor.sulaev@outlook.com>
2024-07-25 22:06:30 +0300

    feat: yazi settings

--- nixpkgs/home-manager/modules/yazi-module/yazi.nix
+++ nixpkgs/home-manager/modules/yazi-module/yazi.nix
@@ -6,6 +6,18 @@
 
   programs.yazi = {
     enable = true;
+
+  settings = {
+    log = {
+      enabled = false;
+    };
+    manager = {
+      show_hidden = false;
+      sort_by = "modified";
+      sort_dir_first = true;
+      sort_reverse = true;
+    };
+  }
   };
 }
 
