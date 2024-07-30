c19af0577ec31160b42e308b0df1d74482e9c818
Fedor Sulaev <fedor.sulaev@outlook.com>
2024-07-30 22:01:00 +0300

    feat: js formatter

--- nixpkgs/home-manager/modules/nvim-module/languages-module/typescript-module/javascript-module/neovim-javascript.nix
+++ nixpkgs/home-manager/modules/nvim-module/languages-module/typescript-module/javascript-module/neovim-javascript.nix
@@ -1,8 +1,14 @@
-{ pkgs, ... }:
+{ pkgs, utility, ... }:
 {
   programs.neovim = {
     extraPackages = with pkgs; [
       nodejs_20
+    ];
+    plugins = with pkgs.vimPlugins; [
+      {
+        plugin = conform-nvim;
+        config = utility.toLuaFile ./conform-formatters-javascript.lua;
+      }
     ];
   };
   home.sessionPath = [ "${pkgs.nodejs_20}/bin" ];
