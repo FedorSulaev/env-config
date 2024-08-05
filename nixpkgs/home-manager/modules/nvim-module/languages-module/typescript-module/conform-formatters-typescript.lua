2b311cdc3cdde14ba1871856526311b94ac4c2e7
Fedor Sulaev <fedor.sulaev@outlook.com>
2024-08-05 19:23:22 +0300

    fix: Line length for prettier

--- nixpkgs/home-manager/modules/nvim-module/languages-module/typescript-module/conform-formatters-typescript.lua
+++ nixpkgs/home-manager/modules/nvim-module/languages-module/typescript-module/conform-formatters-typescript.lua
@@ -1,6 +1,6 @@
 local conform = require("conform")
 conform.formatters.prettier = {
-	prepend_args = { "--tab-width", "4" },
+	prepend_args = { "--tab-width", "4", "--print-width", "120" },
 }
 conform.formatters_by_ft.typescript = { "prettier" }
 conform.formatters_by_ft.typescriptreact = { "prettier" }
