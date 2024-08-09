1a7ff6b12718c1133b2f0c27324a5ad4ae0eba03
Fedor Sulaev <fedor.sulaev@outlook.com>
2024-08-09 20:16:19 +0300

    fix: Timeout error on startup

--- nixpkgs/home-manager/modules/zsh-module/zsh.nix
+++ nixpkgs/home-manager/modules/zsh-module/zsh.nix
@@ -21,7 +21,7 @@
   programs.starship = {
     enable = true;
     settings = {
-      command_timeout = 1000;
+      command_timeout = 2000;
       add_newline = false;
       format = "$directory$character";
       right_format = "$all";
