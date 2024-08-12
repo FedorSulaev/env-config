84df9ba31c2889a84988d9178b76c59190c00448
Fedor Sulaev <fedor.sulaev@outlook.com>
2024-08-12 22:11:51 +0300

    feat: Noice theme config

--- nixpkgs/home-manager/modules/nvim-module/theme-module/theme.lua
+++ nixpkgs/home-manager/modules/nvim-module/theme-module/theme.lua
@@ -6,3 +6,24 @@ vim.cmd.colorscheme("gruvbox")
 
 -- You can configure highlights by doing something like:
 -- vim.cmd.hi("Comment gui=none")
+-- Noice
+vim.api.nvim_set_hl(0, "NoiceCmdlinePopup", { fg = "#fe8019" })
+vim.api.nvim_set_hl(0, "NoiceCmdlinePopupBorder", { fg = "#fe8019" })
+vim.api.nvim_set_hl(0, "NoiceCmdlinePopupBorderCalculator", { fg = "#fe8019" })
+vim.api.nvim_set_hl(0, "NoiceCmdlinePopupBorderCmdline", { fg = "#fe8019" })
+vim.api.nvim_set_hl(0, "NoiceCmdlinePopupBorderFilter", { fg = "#fe8019" })
+vim.api.nvim_set_hl(0, "NoiceCmdlinePopupBorderHelp", { fg = "#fe8019" })
+vim.api.nvim_set_hl(0, "NoiceCmdlinePopupBorderLua", { fg = "#fe8019" })
+vim.api.nvim_set_hl(0, "NoiceCmdlinePopupBorderInput", { fg = "#fe8019" })
+vim.api.nvim_set_hl(0, "NoiceCmdlinePopupBorderIncRename", { fg = "#fe8019" })
+vim.api.nvim_set_hl(0, "NoiceConfirmBorder", { fg = "#fe8019" })
+vim.api.nvim_set_hl(0, "NoicePopupBorder", { fg = "#fe8019" })
+vim.api.nvim_set_hl(0, "NoicePopupmenuBorder", { fg = "#fe8019" })
+vim.api.nvim_set_hl(0, "NoiceSplitBorder", { fg = "#fe8019" })
+
+-- Notify
+vim.api.nvim_set_hl(0, "NotifyDEBUGBorder", { fg = "#fe8019" })
+vim.api.nvim_set_hl(0, "NotifyERRORBorder", { fg = "#cc241d" })
+vim.api.nvim_set_hl(0, "NotifyINFOBorder", { fg = "#fe8019" })
+vim.api.nvim_set_hl(0, "NotifyTRACEBorder", { fg = "#fe8019" })
+vim.api.nvim_set_hl(0, "NotifyWARNBorder", { fg = "#fb4934" })
