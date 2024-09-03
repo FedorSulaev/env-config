a791251914d2d0baff848b2c2a68514f283c13e8
Fedor Sulaev <fedor.sulaev@outlook.com>
2024-09-03 19:42:07 +0300

    refactor: Mason-lspconfig removed

--- nixpkgs/home-manager/modules/nvim-module/lsp-module/neovim-lsp.nix
+++ nixpkgs/home-manager/modules/nvim-module/lsp-module/neovim-lsp.nix
@@ -4,7 +4,6 @@
   ];
   programs.neovim = {
     plugins = with pkgs.vimPlugins; [
-      mason-lspconfig-nvim
       {
         plugin = nvim-lspconfig;
         config = utility.toLuaFile ./lspconfig.lua;
