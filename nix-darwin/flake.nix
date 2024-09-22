34dfac3a1ecdbdd594f0f858f833036194225ddf
Fedor Sulaev <fedor.sulaev@outlook.com>
2024-09-22 19:47:54 +0300

    fix: Home manager missing in darwin flake

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -5,9 +5,13 @@
     nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
     let
       configuration = { pkgs, ... }: {
         # List packages installed in system profile. To search by name, run:
