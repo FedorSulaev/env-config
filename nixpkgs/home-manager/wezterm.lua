9a4a4b03b5af65c3ce681f767c67b8c7f84148c0
Fedor Sulaev <fedor.sulaev@outlook.com>
2024-11-25 23:22:00 +0300

    fix: Graphical bugs

--- nixpkgs/home-manager/wezterm.lua
+++ nixpkgs/home-manager/wezterm.lua
@@ -6,5 +6,7 @@ config.color_scheme = "GruvboxDark"
 config.font = wezterm.font("JetBrainsMono NF", { weight = "DemiBold" })
 config.font_size = 14
 config.hide_tab_bar_if_only_one_tab = true
+config.front_end = "WebGpu"
+config.webgpu_power_preference = "HighPerformance"
 
 return config
