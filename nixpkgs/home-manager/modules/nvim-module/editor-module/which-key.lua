c9bf2fbcf2a75523b2f97d5b3db0f0d524b13e34
Fedor Sulaev <fedor.sulaev@outlook.com>
2024-12-05 20:45:00 +0300

    fix: Obsolete config format for which-key

--- nixpkgs/home-manager/modules/nvim-module/editor-module/which-key.lua
+++ nixpkgs/home-manager/modules/nvim-module/editor-module/which-key.lua
@@ -4,15 +4,15 @@ vim.g.maplocalleader = " "
 local whichKey = require("which-key")
 whichKey.setup()
 -- Document existing key chains
-require("which-key").register({
-	["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-	["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-	["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-	["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-	["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-	["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
+require("which-key").add({
+	{ "<leader>c", name = "[C]ode" },
+	{ "<leader>d", name = "[D]ocument" },
+	{ "<leader>r", name = "[R]ename" },
+	{ "<leader>s", name = "[S]earch" },
+	{ "<leader>w", name = "[W]orkspace" },
+	{ "<leader>h", name = "Git [H]unk" },
 })
 -- Visual mode
-require("which-key").register({
-	["<leader>h"] = { name = "Git [H]unk" },
+require("which-key").add({
+	{ "<leader>h", name = "Git [H]unk" },
 }, { mode = "v" })
