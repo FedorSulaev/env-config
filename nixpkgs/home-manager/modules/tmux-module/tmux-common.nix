9bb9c82cd7367d7b17a29358556bd35ac8ebe3e0
Fedor Sulaev <fedor.sulaev@outlook.com>
2024-12-12 20:11:00 +0300

    feat: Removed cpu plugin

--- nixpkgs/home-manager/modules/tmux-module/tmux-common.nix
+++ nixpkgs/home-manager/modules/tmux-module/tmux-common.nix
@@ -10,12 +10,6 @@
       sensible
       yank
       vim-tmux-navigator
-      {
-        plugin = cpu;
-        extraConfig = ''
-          set -g status-right '#{cpu_bg_color} CPU: #{cpu_icon} #{cpu_percentage} | #{ram_bg_color} MEM: #{ram_icon} #{ram_percentage} | %a %h-%d %H:%M '
-        '';
-      }
       gruvbox
     ];
     extraConfig = ''
