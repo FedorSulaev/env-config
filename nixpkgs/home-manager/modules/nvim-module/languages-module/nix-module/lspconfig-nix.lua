bf84435d0d4e882d78a7c4e67d594d678b60a70b
Fedor Sulaev <fedor.sulaev@outlook.com>
2024-12-14 19:39:33 +0200

    feat: nixd config

--- nixpkgs/home-manager/modules/nvim-module/languages-module/nix-module/lspconfig-nix.lua
+++ nixpkgs/home-manager/modules/nvim-module/languages-module/nix-module/lspconfig-nix.lua
@@ -3,4 +3,15 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 lspconfig["nixd"].setup({
 	capabilities = capabilities,
+	cmd = { "nixd" },
+	settings = {
+		nixd = {
+			nixpkgs = {
+				expr = "import <nixpkgs> { }",
+			},
+			formatting = {
+				command = { "nixpkgs-fmt" },
+			},
+		},
+	},
 })
