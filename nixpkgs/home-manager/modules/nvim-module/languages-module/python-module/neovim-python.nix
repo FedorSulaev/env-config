acb222da6179f6d4fa334729091e9c4421c33cf8
Fedor Sulaev <fedor.sulaev@outlook.com>
2024-12-17 19:01:13 +0200

    feat: Custom import for contextily

--- nixpkgs/home-manager/modules/nvim-module/languages-module/python-module/neovim-python.nix
+++ nixpkgs/home-manager/modules/nvim-module/languages-module/python-module/neovim-python.nix
@@ -1,5 +1,36 @@
 { pkgs, ... }:
+let
+  buildPythonPackage = pkgs.python3Packages.buildPythonPackage;
+  fetchPypi = pkgs.python3Packages.fetchPypi;
+  contextily = buildPythonPackage
+    rec {
+      pname = "contextily";
+      version = "1.6.2";
+
+      doCheck = false;
+      dontUseCmakeConfigure = true;
+      format = "pyproject";
+
+      src = fetchPypi {
+        inherit pname version;
+        sha256 = "sha256-PHR5JSab4kipsadhhZ4F0WgShuBNXQeWva8d40CUdLs=";
+      };
+
+      propagatedBuildInputs = with pkgs.python3Packages; [
+        setuptools_scm
+        geopy
+        matplotlib
+        mercantile
+        pillow
+        rasterio
+        requests
+        joblib
+        xyzservices
+      ];
+    };
+in
 {
+
   home.packages = with pkgs; [
     (python3.withPackages (ps: with ps; [
       ipython
@@ -8,6 +39,7 @@
       pandas
       matplotlib
       seaborn
+      contextily
     ]))
   ];
   programs.neovim = {
