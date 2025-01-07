5b29d5dcbcc59f5e9b078a2261b38262b281f4c9
Fedor Sulaev <fedor.sulaev@outlook.com>
2025-01-07 14:53:29 +0200

    fix: Default editor

--- nixpkgs/home-manager/modules/nvim-module/settings-module/neovim-settings.nix
+++ nixpkgs/home-manager/modules/nvim-module/settings-module/neovim-settings.nix
@@ -6,6 +6,7 @@
       viAlias = true;
       vimAlias = true;
       vimdiffAlias = true;
+      defaultEditor = true;
       extraLuaConfig = ''
         ${builtins.readFile ./globals.lua}
         ${builtins.readFile ./options.lua}
