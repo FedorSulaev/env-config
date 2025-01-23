0fd1b3e56036244242ec3f3efab7bd4cb453b81e
Fedor Sulaev <fedor.sulaev@outlook.com>
2025-01-23 06:58:00 +0200

    feat: Quickfix keymap

--- nixpkgs/home-manager/modules/nvim-module/settings-module/keymaps.lua
+++ nixpkgs/home-manager/modules/nvim-module/settings-module/keymaps.lua
@@ -42,11 +42,11 @@ vim.keymap.set("n", "<C-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>")
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 -- buffers
@@ -58,3 +58,7 @@ vim.api.nvim_set_keymap("n", "<C-x>", ":bdelete<enter>", { noremap = false })
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+-- quickfix
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
