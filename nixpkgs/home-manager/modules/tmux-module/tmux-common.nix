ab99ff52cf4ab8c87279896ddff16347b7d5190f
Fedor Sulaev <fedor.sulaev@outlook.com>
2025-02-16 16:37:49 +0200

    fix: Continuum not working, order of tmux loading

--- nixpkgs/home-manager/modules/tmux-module/tmux-common.nix
+++ nixpkgs/home-manager/modules/tmux-module/tmux-common.nix
@@ -7,34 +7,44 @@
     keyMode = "vi";
     prefix = "C-s";
     plugins = with pkgs.tmuxPlugins; [
-      sensible
+      {
+        plugin = sensible;
+        extraConfig = ''
+          set-option -g default-command zsh
+        '';
+      }
       yank
-      vim-tmux-navigator
-      gruvbox
+      {
+        plugin = vim-tmux-navigator;
+        extraConfig = ''
+          bind-key h select-pane -L
+          bind-key j select-pane -D
+          bind-key k select-pane -U
+          bind-key l select-pane -R
+        '';
+      }
+      {
+        plugin = gruvbox;
+        extraConfig = ''
+          set -g status-position top
+          # For preview rendering
+          set -g allow-passthrough on
+          set -ga update-environment TERM
+          set -ga update-environment TERM_PROGRAM
+
+          set -g default-terminal 'tmux-256color'
+          set-option -sa terminal-features ',*:RGB'
+        '';
+      }
       resurrect
-      continuum
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-save-interval '10'
+          set -g @continuum-restore 'on'
+        '';
+      }
     ];
-    extraConfig = ''
-      set-option -g default-command zsh
-
-      bind-key h select-pane -L
-      bind-key j select-pane -D
-      bind-key k select-pane -U
-      bind-key l select-pane -R
-
-      set -g status-position top
-
-      # For preview rendering
-      set -g allow-passthrough on
-      set -ga update-environment TERM
-      set -ga update-environment TERM_PROGRAM
-
-      set -g default-terminal 'tmux-256color'
-      set-option -sa terminal-features ',*:RGB'
-
-      # Persistent sessions
-      set -g @continuum-restore on
-    '';
   };
 
   home.packages = with pkgs; [
