2f666b1b4da458bfa11ada61891f98202b7480f7
Fedor Sulaev <fedor.sulaev@outlook.com>
2025-02-24 19:27:00 +0300

    feat: Sesh zsh shortcut

--- nixpkgs/home-manager/modules/tmux-module/tmux-common.nix
+++ nixpkgs/home-manager/modules/tmux-module/tmux-common.nix
@@ -73,8 +73,25 @@
     enable = true;
   };
 
-  home.packages = with pkgs; [
-    tmuxifier
-    sesh
-  ];
+  programs.zsh = {
+    initExtra = ''
+      function sesh-sessions() {
+        {
+          exec </dev/tty
+          exec <&1
+          local session
+          session=$(sesh list -t -c | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
+          zle reset-prompt > /dev/null 2>&1 || true
+          [[ -z "$session" ]] && return
+          sesh connect $session
+        }
+      }
+    '';
+  };
+
+  home.packages = with pkgs;
+    [
+      tmuxifier
+      sesh
+    ];
 }
