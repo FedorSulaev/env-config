65aca06dbc7507ab33e18cc7fa230445fcdcd502
Fedor Sulaev <1536446+FedorSulaev@users.noreply.github.com>
2025-05-05 20:08:34 +0300

    feat: Host spec

---
+++ common/utility/host-spec.nix
@@ -0,0 +1,56 @@
+{ config, pkgs, lib, ... }:
+{
+  options.hostSpec = {
+    username = lib.mkOption {
+      type = lib.types.str;
+      description = "Main username for the host";
+    };
+    hostName = lib.mkOption {
+      type = lib.types.str;
+      description = "The hostname of the host";
+    };
+    email = lib.mkOption {
+      type = lib.types.attrsOf lib.types.str;
+      description = "The email of the user";
+    };
+    networking = lib.mkOption {
+      default = { };
+      type = lib.types.attrsOf lib.types.anything;
+      description = "An attribute set of networking information";
+    };
+    wifi = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Used to indicate if a host has wifi";
+    };
+    domain = lib.mkOption {
+      type = lib.types.str;
+      description = "The domain of the host";
+    };
+    userFullName = lib.mkOption {
+      type = lib.types.str;
+      description = "The full name of the user";
+    };
+    handle = lib.mkOption {
+      type = lib.types.str;
+      description = "The handle of the user (eg: github user)";
+    };
+    home = lib.mkOption {
+      type = lib.types.str;
+      description = "The home directory of the user";
+      default =
+        let
+          user = config.hostSpec.username;
+        in
+        if pkgs.stdenv.isLinux then "/home/${user}" else "/Users/${user}";
+    };
+  };
+
+  config = {
+    assertions =
+      let
+      in
+      [
+      ];
+  };
+}
