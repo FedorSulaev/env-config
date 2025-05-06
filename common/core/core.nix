6f55645f1092bc83c6a875f1b77d86f648669ba4
Fedor Sulaev <1536446+FedorSulaev@users.noreply.github.com>
2025-05-06 19:24:37 +0300

    feat: Base hm config

---
+++ common/core/core.nix
@@ -0,0 +1,24 @@
+{ config, lib, pkgs, hostSpec, ... }:
+{
+  inherit hostSpec;
+
+  services.ssh-agent.enable = true;
+
+  home = {
+    username = lib.mkDefault config.hostSpec.username;
+    homeDirectory = lib.mkDefault config.hostSpec.home;
+    stateVersion = lib.mkDefault "24.11";
+    sessionPath = [
+    ];
+    sessionVariables = {
+      FLAKE = "$HOME/env-config";
+      SHELL = "zsh";
+      TERM = "";
+      TERMINAL = "";
+      VISUAL = "nvim";
+      EDITOR = "nvim";
+      MANPAGER = "batman";
+    };
+    preferXdgDirectories = true;
+  };
+}
