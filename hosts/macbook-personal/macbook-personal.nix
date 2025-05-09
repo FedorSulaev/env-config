07b8044ef4042d11b652653e9d1b33986a14236c
Fedor Sulaev <1536446+FedorSulaev@users.noreply.github.com>
2025-05-09 20:01:00 +0300

    feat: Minimal mac config

--- hosts/macbook-personal/macbook-personal.nix
+++ hosts/macbook-personal/macbook-personal.nix
@@ -1,3 +1,13 @@
 { pkgs, ... }:
 {
+  nix.settings.experimental-features = "nix-command flakes";
+  nix.buildMachines = [{
+    system = "x86_64-linux";
+    protocol = "ssh-ng";
+    maxJobs = 4;
+    speedFactor = 1;
+    supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+  }];
+  nix.distributedBuilds = true;
+  system.stateVersion = 5;
 }
