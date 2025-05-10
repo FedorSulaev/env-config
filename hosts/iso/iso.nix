5b4fe1b85200eef6021bf44cafb5afe377d6eb52
Fedor Sulaev <1536446+FedorSulaev@users.noreply.github.com>
2025-05-10 18:23:52 +0300

    feat: Minimal iso config

--- hosts/iso/iso.nix
+++ hosts/iso/iso.nix
@@ -1,3 +1,13 @@
 { inputs, pkgs, lib, config, ... }:
 {
+  imports = lib.flatten [
+    "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+    #"${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix"
+    "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
+    # This is overkill but I want my core home level utils if I need to use the iso environment for recovery purpose
+    inputs.home-manager.nixosModules.home-manager
+    ../../common/utility/host-spec.nix
+    ../../common/users/user-primary.nix
+    ../../common/users/temp-password.nix
+  ];
 }
