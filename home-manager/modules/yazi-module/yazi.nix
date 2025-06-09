85958858773f5e4fef39a4f1b52e4a9bbed2eb5d
Fedor Sulaev <1536446+FedorSulaev@users.noreply.github.com>
2025-06-09 18:10:04 +0300

    fix: Yazi config format

--- home-manager/modules/yazi-module/yazi.nix
+++ home-manager/modules/yazi-module/yazi.nix
@@ -11,9 +11,9 @@
       log = {
         enabled = false;
       };
-      manager = {
+      mgr = {
         show_hidden = true;
-        sort_by = "modified";
+        sort_by = "mtime";
         sort_dir_first = true;
         sort_reverse = true;
       };
