e4544f0bd9d393c724ff7d40224707f5fa9f2b0c
Fedor Sulaev <1536446+FedorSulaev@users.noreply.github.com>
2025-07-23 19:08:00 +0300

    refactor: Project structure

--- hosts/common/home-manager/modules/tmux-module/tmux-breezora.nix
+++ hosts/common/home-manager/modules/tmux-module/tmux-breezora.nix
@@ -5,4 +5,5 @@
   ];
 
   home.file.".tmux-layouts/envconfig.session.sh".source = ./tmux-session-envconfig.sh;
+  home.file.".tmux-layouts/selfhost.session.sh".source = ./tmux-session-selfhost.sh;
 }
