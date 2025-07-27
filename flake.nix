3b912b47abadf03e6fa7fb4262078c80614a72f2
Fedor Sulaev <1536446+FedorSulaev@users.noreply.github.com>
2025-07-27 15:42:25 +0300

    feat: Hardware configurations input

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
+    hardware.url = "github:NixOS/nixos-hardware";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
