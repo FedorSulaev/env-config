84f99ab53bda4f45c68dfe86a7f9f993bf7f829a
Fedor Sulaev <1536446+FedorSulaev@users.noreply.github.com>
2025-07-27 16:17:54 +0300

    feat: Boot config

--- hosts/stonebark/stonebark.nix
+++ hosts/stonebark/stonebark.nix
@@ -1,6 +1,29 @@
 { inputs, lib, config, pkgs, ... }:
 {
   imports = [
+    # ===== Hardware =====
     ./hardware-configuration.nix
+    inputs.hardware.nixosModules.common-cpu-amd
+    inputs.hardware.nixosModules.common-pc-ssd
   ];
+
+  boot = {
+    kernelParams = [
+      "amd_iommu=on"
+      "iommu=pt"
+    ];
+    kernelModules = [ "vfio" "vfio_pci" "vfio_iommu_type1" "vfio_virqfd" ];
+    loader.grub = {
+      enable = true;
+      version = 2;
+      devices = [ "/dev/nvme0n1" ];
+    };
+    blacklistedKernelModules = [ "nouveau" "nvidia" ];
+    extraModprobeConfig = ''
+      options vfio-pci ids=10de:2484,10de:228b
+    '';
+  };
+
+  services.qemuGuest.enable = true;
+  virtualisation.libvirtd.enable = true;
 }
