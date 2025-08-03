6094dc41872f008e4b2ddeadbe8a71643dd066d5
Fedor Sulaev <1536446+FedorSulaev@users.noreply.github.com>
2025-08-03 18:39:19 +0300

    feat: Bootstrap args handler

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -15,3 +15,27 @@ function cleanup() {
     echo "Temp directory is cleaned up"
 }
 trap cleanup exit
+
+# Usage help function
+function help_and_exit() {
+    echo
+    echo "Remotely bootstraps new host from this nix-config"
+    echo
+    echo "USAGE: "
+    exit 0
+}
+
+# Handle command-line arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -n)
+            shift
+            echo "-n"
+            ;;
+        *)
+            red "ERROR: Invalid argument."
+            help_and_exit
+            ;;
+    esac
+    shift
+done
