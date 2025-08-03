04f0f03f5c6891e5234b24000561409fcd91b3ed
Fedor Sulaev <1536446+FedorSulaev@users.noreply.github.com>
2025-08-03 18:17:07 +0300

    feat: Bootstrap temp dir

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -4,3 +4,14 @@ set -euo pipefail
 # Helpers library
 # shellcheck disable=SC1091
 source "$(dirname "${BASH_SOURCE[0]}")/helpers.sh"
+
+# Create a temp directory for generated host keys
+temp=$(mktemp -d)
+echo "Temp directory is: $temp"
+
+# Cleanup temporary directory on exit
+function cleanup() {
+    rm -rf "$temp"
+    echo "Temp directory is cleaned up"
+}
+trap cleanup exit
