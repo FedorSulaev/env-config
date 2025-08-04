7fa78122967035de6b4d6581a427303c75ebe949
Fedor Sulaev <1536446+FedorSulaev@users.noreply.github.com>
2025-08-04 18:29:00 +0300

    feat: Bootstrap - target hostname arg

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -5,6 +5,9 @@ set -euo pipefail
 # shellcheck disable=SC1091
 source "$(dirname "${BASH_SOURCE[0]}")/helpers.sh"
 
+# User variables
+target_hostname=""
+
 # Create a temp directory for generated host keys
 temp=$(mktemp -d)
 echo "Temp directory is: $temp"
@@ -21,7 +24,12 @@ function help_and_exit() {
     echo
     echo "Remotely bootstraps new host from this nix-config"
     echo
-    echo "USAGE: "
+    echo "USAGE: $0 -n <target_hostname> [OPTIONS]"
+    echo
+    echo "ARGS:"
+    echo "  -n <target_hostname>                specify target_hostname of the host to deploy the config on."
+    echo
+    echo "OPTIONS:"
     exit 0
 }
 
@@ -30,7 +38,7 @@ while [[ $# -gt 0 ]]; do
     case "$1" in
         -n)
             shift
-            echo "-n"
+            target_hostname=$1
             ;;
         *)
             red "ERROR: Invalid argument."
@@ -39,3 +47,9 @@ while [[ $# -gt 0 ]]; do
     esac
     shift
 done
+
+if [ -z "$target_hostname" ]; then
+    red "ERROR: -n is required"
+    echo
+    help_and_exit
+fi
