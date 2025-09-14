f91ddbb5b33d49277551c96362a1257d3dc19c1e
Fedor Sulaev <1536446+FedorSulaev@users.noreply.github.com>
2025-09-14 17:17:09 +0300

    feat: Minimal flake inputs

--- bootstrap/flake.nix
+++ bootstrap/flake.nix
@@ -1,3 +1,8 @@
 {
   description = "Minimal NixOS configuration for bootstrapping systems";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    disko.url = "github:nix-community/disko";
+  };
 }
