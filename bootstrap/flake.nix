31c5fb05557d98121f02769634e49d789b5c771f
Fedor Sulaev <1536446+FedorSulaev@users.noreply.github.com>
2025-09-30 06:30:16 +0300

    feat: Minimal config

--- bootstrap/flake.nix
+++ bootstrap/flake.nix
@@ -5,4 +5,54 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     disko.url = "github:nix-community/disko";
   };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      inherit (self) outputs;
+
+      minimalSpecialArgs = {
+        inherit inputs outputs;
+      };
+      newConfig =
+        name: disk: swapSize: useLuks: useImpermanence:
+        (
+          let
+            diskSpecPath =
+              if useLuks && useImpermanence then
+                ../hosts/common/disks/luks-impermanence-disk.nix
+              else if !useLuks && useImpermanence then
+                ../hosts/common/disks/impermanence-disk.nix
+              else
+                ../hosts/common/disks/host-disk.nix;
+          in
+          nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            specialArgs = minimalSpecialArgs;
+            modules = [
+              inputs.disko.nixosModules.disko
+              diskSpecPath
+              {
+                _module.args = {
+                  inherit disk;
+                  withSwap = swapSize > 0;
+                  swapSize = builtins.toString swapSize;
+                };
+              }
+              ./minimal-configuration.nix
+              #../hosts/nixos/${name}/hardware-configuration.nix
+              { networking.hostName = name; }
+            ];
+          }
+        );
+    in
+    {
+      nixosConfigurations = {
+        # host = newConfig "name" disk" "swapSize" "useLuks" "useImpermanence"
+        # Swap size is in GiB
+        personalapps = newConfig "personalapps" "/dev/vda" 0 false false;
+        testlab = newConfig "testlab" "/dev/vda" 0 false false;
+        test = newConfig "test" "/dev/vda" 0 false false;
+        stonebark = newConfig "stonebark" "/dev/vda" 0 false false;
+      };
+    };
 }
--- bootstrap/minimal-configuration.nix
+++ bootstrap/minimal-configuration.nix
@@ -1,3 +1,78 @@
 { inputs, config, lib, pkgs, ... }:
 {
+  imports = [
+    ../common/utility/host-spec.nix
+  ];
+
+  hostSpec = {
+    hostName = "installer";
+    username = "fedors";
+  };
+
+  fileSystems."/boot".options = [ "umask=0077" ]; # Removes permissions and security warnings.
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    configurationLimit = lib.mkDefault 3;
+    consoleMode = lib.mkDefault "max";
+  };
+  boot.initrd = {
+    systemd.enable = true;
+    systemd.emergencyAccess = true;
+    luks.forceLuksSupportInInitrd = true;
+  };
+  boot.kernelParams = [
+    "systemd.setenv=SYSTEMD_SULOGIN_FORCE=1"
+    "systemd.show_status=true"
+    #"systemd.log_level=debug"
+    "systemd.log_target=console"
+    "systemd.journald.forward_to_console=1"
+  ];
+
+  security.pam = {
+    rssh.enable = true;
+    services.sudo = {
+      rssh = true;
+      u2fAuth = true;
+    };
+  };
+
+  environment.systemPackages = builtins.attrValues {
+    inherit (pkgs)
+      wget
+      curl
+      rsync
+      git
+      ;
+  };
+
+  networking = {
+    networkmanager.enable = true;
+  };
+
+  services = {
+    qemuGuest.enable = true;
+    openssh =
+      {
+        enable = true;
+        ports = [ 22 ];
+        settings.PermitRootLogin = "yes";
+        authorizedKeysFiles = lib.mkForce [ "/etc/ssh/authorized_keys.d/%u" ];
+      };
+  };
+
+  nix = {
+    #FIXME(installer): registry and nixPath shouldn't be required here because flakes but removal results in warning spam on build
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      warn-dirty = false;
+    };
+  };
+
+  system.stateVersion = "25.05";
 }
