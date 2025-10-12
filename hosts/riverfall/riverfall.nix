c9880d5860714a2b9d1dbb6d9c4868fe20d7ec96
Fedor Sulaev <1536446+FedorSulaev@users.noreply.github.com>
2025-10-12 14:53:37 +0300

    feat: Initial config

--- hosts/riverfall/riverfall.nix
+++ hosts/riverfall/riverfall.nix
@@ -1,2 +1,10 @@
-{ ... }:
-{ }
+{ pkgs, ... }:
+{
+  imports = [
+    "${pkgs.path}/nixos/modules/virtualisation/qemu-vm.nix"
+  ];
+
+  virtualisation.qemu.diskInterface = "virtio";
+  virtualisation.qemu.memorySize = 2048;
+  virtualisation.qemu.networkingOptions = [ "bridge=br0" ];
+}
