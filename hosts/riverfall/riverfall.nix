e9eec7a2ced6886c847bde40895b06e605dc93c7
Fedor Sulaev <1536446+FedorSulaev@users.noreply.github.com>
2025-10-12 15:24:33 +0300

    fix: VM options

--- hosts/riverfall/riverfall.nix
+++ hosts/riverfall/riverfall.nix
@@ -1,2 +1,11 @@
 { pkgs, ... }:
-{ }
+{
+  virtualisation.qemu.options = [
+    "-m"
+    "2048"
+    "-device"
+    "virtio-net-pci,netdev=net0"
+    "-netdev"
+    "bridge,id=net0,br=br0"
+  ];
+}
