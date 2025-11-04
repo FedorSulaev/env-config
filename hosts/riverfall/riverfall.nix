42b354d6cf702189a6c43427a7a34e7c6817dc79
Fedor Sulaev <1536446+FedorSulaev@users.noreply.github.com>
2025-11-04 20:02:00 +0300

    feat: Container for Actual

--- hosts/riverfall/riverfall.nix
+++ hosts/riverfall/riverfall.nix
@@ -35,6 +35,9 @@
       interface = "enp1s0";
     };
     nameservers = config.hostSpec.networking.hosts.stonebark.nameservers;
+    firewall.allowedTCPPorts = [
+      5006 # ActualBudget
+    ];
   };
 
   users = {
@@ -60,6 +63,46 @@
   environment.systemPackages = with pkgs; [
     vim
   ];
+
+  virtualisation.containers.enable = true;
+  virtualisation = {
+    podman = {
+      enable = true;
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+      autoPrune = {
+        enable = true;
+        dates = "weekly";
+        flags = [
+          "--filter=until=24h"
+          "--filter=label!=important"
+        ];
+      };
+    };
+    oci-containers.backend = "podman";
+    oci-containers.containers = {
+      actualBudget = {
+        image = "docker.io/actualbudget/actual-server:latest-alpine";
+        autoStart = true;
+        environment = {
+          # Environment variables for the Actual Server
+          ACTUAL_HTTPS_KEY = "/data/selfhost.key";
+          ACTUAL_HTTPS_CERT = "/data/selfhost.crt";
+          # Uncomment and customize additional options if needed
+          # ACTUAL_PORT = "5006";
+          # ACTUAL_UPLOAD_FILE_SYNC_SIZE_LIMIT_MB = "20";
+          # ACTUAL_UPLOAD_SYNC_ENCRYPTED_FILE_SYNC_SIZE_LIMIT_MB = "50";
+          # ACTUAL_UPLOAD_FILE_SIZE_LIMIT_MB = "20";
+        };
+        ports = [ "5006:5006" ];
+        volumes = [
+          "/home/fedors/actual-data:/data"
+        ];
+      };
+    };
+  };
 
   system.stateVersion = "25.05";
 }
