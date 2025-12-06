645c47bf1a5439361124908b04e57bef7a4f2f0a
Fedor Sulaev <1536446+FedorSulaev@users.noreply.github.com>
2025-12-06 18:53:21 +0200

    feat: Initial HM setup

--- hosts/sunpeak/sunpeak-home.nix
+++ hosts/sunpeak/sunpeak-home.nix
@@ -11,5 +11,34 @@
   home.username = config.hostSpec.username;
   home.homeDirectory = "/home/${config.hostSpec.username}";
   home.stateVersion = "25.05";
+  home.sessionVariables = {
+    MOZ_ENABLE_WAYLAND = "1";
+    NIXOS_OZONE_WL = "1";
+  };
+
+  programs.kitty.enable = true;
+
+  programs.wofi.enable = true;
+
+  home.packages = with pkgs; [
+    gnome-tweaks
+    gnomeExtensions.appindicator
+    git
+    neovim
+    htop
+    ripgrep
+    fd
+    jq
+    firefox
+    blanket
+    bitwarden-desktop
+    bitwarden-cli
+  ];
+
+  programs.zsh = {
+    enable = true;
+    oh-my-zsh.enable = true;
+    oh-my-zsh.theme = "agnoster";
+  };
 }
 
