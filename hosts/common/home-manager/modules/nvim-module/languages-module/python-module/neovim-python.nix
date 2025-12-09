b866f6281f2010f7035e7a3e90dcc11bbb5e99dc
Fedor Sulaev <1536446+FedorSulaev@users.noreply.github.com>
2025-12-09 08:14:55 +0200

    chore: Python dependency cleanup

--- hosts/common/home-manager/modules/nvim-module/languages-module/python-module/neovim-python.nix
+++ hosts/common/home-manager/modules/nvim-module/languages-module/python-module/neovim-python.nix
@@ -1,36 +1,5 @@
 { pkgs, ... }:
-let
-  buildPythonPackage = pkgs.python313Packages.buildPythonPackage;
-  fetchPypi = pkgs.python313Packages.fetchPypi;
-  contextily = buildPythonPackage
-    rec {
-      pname = "contextily";
-      version = "1.7.0";
-
-      doCheck = false;
-      dontUseCmakeConfigure = true;
-      format = "pyproject";
-
-      src = fetchPypi {
-        inherit pname version;
-        sha256 = "sha256:6534faa5702b89b46d0d81b4c538754f2d8b3dd8cc298454b11ccedfa67e73ac";
-      };
-
-      propagatedBuildInputs = with pkgs.python313Packages; [
-        setuptools-scm
-        geopy
-        matplotlib
-        mercantile
-        pillow
-        rasterio
-        requests
-        joblib
-        xyzservices
-      ];
-    };
-in
 {
-
   home.packages = with pkgs; [
     (python313.withPackages (ps: with ps; [
       ipython
@@ -39,7 +8,6 @@ in
       pandas
       matplotlib
       seaborn
-      contextily
       geopandas
       scipy
       sklearn-compat
