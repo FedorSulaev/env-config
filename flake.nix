99d4e089585dbb03f00989a7ce84ce9deb0e796c
Fedor Sulaev <1536446+FedorSulaev@users.noreply.github.com>
2026-02-26 18:43:00 +0300

    refactor: flake-parts wrapper

--- flake.nix
+++ flake.nix
@@ -28,11 +28,9 @@
     };
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     import-tree = {
       url = "github:vic/import-tree";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     wrapper-modules = {
       url = "github:BirdeeHub/nix-wrapper-modules";
@@ -40,132 +38,153 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nix-darwin, sops-nix, ... }@inputs:
-    let
-      overlays = [
-        (final: prev: { })
+  outputs = inputs@{ flake-parts, nixpkgs, home-manager, nix-darwin, sops-nix, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
       ];
 
-      mkPkgs = system: import nixpkgs {
-        inherit system overlays;
-        config.allowUnfree = true;
-      };
-      pkgs-mac-arm = mkPkgs "aarch64-darwin";
-      pkgs-linux-x86 = mkPkgs "x86_64-linux";
-
-      mkNixos = modules: nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        pkgs = pkgs-linux-x86;
-        specialArgs = { inherit inputs; };
-        inherit modules;
-      };
-
-      vmDefs = {
-        riverfall = {
-          modules = [
-            sops-nix.nixosModules.sops
-            ./hosts/riverfall/riverfall.nix
-            ./hosts/riverfall/riverfall-qcow.nix
-          ];
-          diskSize = 40960;
-          imageName = "riverfall-qcow2";
-        };
-        sunpeak = {
-          modules = [
-            ./hosts/sunpeak/sunpeak.nix
-            ./hosts/sunpeak/sunpeak-qcow.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                extraSpecialArgs = { inherit inputs; };
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                backupFileExtension = "hm-backup";
-                users."${inputs.env-secrets.sunpeak.username}" =
-                  ./hosts/sunpeak/sunpeak-home.nix;
-              };
-            }
-          ];
-          diskSize = 40960;
-          imageName = "sunpeak-qcow2";
-        };
-        thornhollow = {
-          modules = [
-            sops-nix.nixosModules.sops
-            ./hosts/thornhollow/thornhollow.nix
-            ./hosts/thornhollow/thornhollow-qcow.nix
-          ];
-          diskSize = 8192;
-          imageName = "thornhollow-qcow2";
-        };
-      };
-
-      mkVMImage = { imageName, modules, diskSize }:
-        let
-          vmConfig = mkNixos modules;
-        in
-        import "${nixpkgs}/nixos/lib/make-disk-image.nix" {
-          pkgs = pkgs-linux-x86;
-          lib = pkgs-linux-x86.lib;
-          inherit diskSize;
-          name = imageName;
-          format = "qcow2";
-          config = vmConfig.config;
-        };
-    in
-    {
-      homeConfigurations.DevDsk = inputs.home-manager.lib.homeManagerConfiguration {
-        pkgs = pkgs-linux-x86;
-        modules = [
-          ./hosts/dev-dsk/dev-dsk-home-manager.nix
-        ];
-        extraSpecialArgs = { inherit inputs; };
-      };
-
-      darwinConfigurations = {
-        breezora = nix-darwin.lib.darwinSystem {
-          system = "aarch64-darwin";
-          pkgs = pkgs-mac-arm;
-          modules = [
-            ./hosts/breezora/breezora.nix
-            home-manager.darwinModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                extraSpecialArgs = { inherit inputs; };
-                users.fedorsulaev = ./hosts/breezora/breezora-home-manager.nix;
-              };
-            }
-          ];
-          inputs = inputs;
-        };
-      };
-
-      nixosConfigurations = {
-        iso = mkNixos [ ./hosts/iso/iso.nix ];
-        stonebark = mkNixos [
-          inputs.disko.nixosModules.disko
-          inputs.NixVirt.nixosModules.default
-          ./hosts/common/disks/host-disk.nix
-          ./hosts/stonebark/stonebark.nix
-        ];
-      }
-      // nixpkgs.lib.mapAttrs (_name: def: mkNixos def.modules) vmDefs;
-
-      packages.x86_64-linux = nixpkgs.lib.mapAttrs'
-        (vmName: def:
+      imports = [
+        ({ lib, ... }:
           let
-            imageName = def.imageName or "${vmName}-qcow2";
+            overlays = [
+              (final: prev: { })
+            ];
+
+            mkPkgs = system: import nixpkgs {
+              inherit system overlays;
+              config.allowUnfree = true;
+            };
+
+            pkgs-mac-arm = mkPkgs "aarch64-darwin";
+            pkgs-linux-x86 = mkPkgs "x86_64-linux";
+
+            mkNixos = modules: nixpkgs.lib.nixosSystem {
+              system = "x86_64-linux";
+              pkgs = pkgs-linux-x86;
+              specialArgs = { inherit inputs; };
+              inherit modules;
+            };
+
+            vmDefs = {
+              riverfall = {
+                modules = [
+                  sops-nix.nixosModules.sops
+                  ./hosts/riverfall/riverfall.nix
+                  ./hosts/riverfall/riverfall-qcow.nix
+                ];
+                diskSize = 40960;
+                imageName = "riverfall-qcow2";
+              };
+              sunpeak = {
+                modules = [
+                  ./hosts/sunpeak/sunpeak.nix
+                  ./hosts/sunpeak/sunpeak-qcow.nix
+                  home-manager.nixosModules.home-manager
+                  {
+                    home-manager = {
+                      extraSpecialArgs = { inherit inputs; };
+                      useGlobalPkgs = true;
+                      useUserPackages = true;
+                      backupFileExtension = "hm-backup";
+                      users."${inputs.env-secrets.sunpeak.username}" =
+                        ./hosts/sunpeak/sunpeak-home.nix;
+                    };
+                  }
+                ];
+                diskSize = 40960;
+                imageName = "sunpeak-qcow2";
+              };
+              thornhollow = {
+                modules = [
+                  sops-nix.nixosModules.sops
+                  ./hosts/thornhollow/thornhollow.nix
+                  ./hosts/thornhollow/thornhollow-qcow.nix
+                ];
+                diskSize = 8192;
+                imageName = "thornhollow-qcow2";
+              };
+            };
+
+            mkVMImage = { imageName, modules, diskSize }:
+              let
+                vmConfig = mkNixos modules;
+              in
+              import "${nixpkgs}/nixos/lib/make-disk-image.nix" {
+                pkgs = pkgs-linux-x86;
+                lib = pkgs-linux-x86.lib;
+                inherit diskSize;
+                name = imageName;
+                format = "qcow2";
+                config = vmConfig.config;
+              };
           in
           {
-            name = imageName;
-            value = mkVMImage {
-              inherit imageName;
-              inherit (def) modules diskSize;
+            flake = {
+              homeConfigurations.DevDsk =
+                inputs.home-manager.lib.homeManagerConfiguration {
+                  pkgs = pkgs-linux-x86;
+                  modules = [
+                    ./hosts/dev-dsk/dev-dsk-home-manager.nix
+                  ];
+                  extraSpecialArgs = { inherit inputs; };
+                };
+
+              darwinConfigurations = {
+                breezora = nix-darwin.lib.darwinSystem {
+                  system = "aarch64-darwin";
+                  pkgs = pkgs-mac-arm;
+                  modules = [
+                    ./hosts/breezora/breezora.nix
+                    home-manager.darwinModules.home-manager
+                    {
+                      home-manager = {
+                        useGlobalPkgs = true;
+                        useUserPackages = true;
+                        extraSpecialArgs = { inherit inputs; };
+                        users.fedorsulaev =
+                          ./hosts/breezora/breezora-home-manager.nix;
+                      };
+                    }
+                  ];
+                  inputs = inputs;
+                };
+              };
+
+              nixosConfigurations =
+                {
+                  iso = mkNixos [ ./hosts/iso/iso.nix ];
+                  stonebark = mkNixos [
+                    inputs.disko.nixosModules.disko
+                    inputs.NixVirt.nixosModules.default
+                    ./hosts/common/disks/host-disk.nix
+                    ./hosts/stonebark/stonebark.nix
+                  ];
+                }
+                // nixpkgs.lib.mapAttrs (_name: def: mkNixos def.modules) vmDefs;
+
+              packages.x86_64-linux =
+                nixpkgs.lib.mapAttrs'
+                  (vmName: def:
+                    let
+                      imageName = def.imageName or "${vmName}-qcow2";
+                    in
+                    {
+                      name = imageName;
+                      value = mkVMImage {
+                        inherit imageName;
+                        inherit (def) modules diskSize;
+                      };
+                    }
+                  )
+                  vmDefs;
             };
-          }
-        )
-        vmDefs;
+          })
+      ];
     };
 }
+
+
+
+
