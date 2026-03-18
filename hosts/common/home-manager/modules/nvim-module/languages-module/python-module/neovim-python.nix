be91eb9a8df9d5453b269b9630cda85550e0b3b4
Fedor Sulaev <1536446+FedorSulaev@users.noreply.github.com>
2026-03-18 19:23:00 +0300

    feat: aif360 package

--- hosts/common/home-manager/modules/nvim-module/languages-module/python-module/neovim-python.nix
+++ hosts/common/home-manager/modules/nvim-module/languages-module/python-module/neovim-python.nix
@@ -1,16 +1,46 @@
 { pkgs, ... }:
+
+let
+  python = pkgs.python313;
+
+  pythonPackages = python.pkgs;
+
+  aif360 = pythonPackages.buildPythonPackage rec {
+    pname = "aif360";
+    version = "0.6.1";
+    pyproject = true;
+    src = pythonPackages.fetchPypi {
+      inherit pname version;
+      hash = "sha256-Y1r8C754Xgj6JC7uXlCAI4GT0Fg7wPUAahMgUNT7d/I=";
+    };
+    build-system = with pythonPackages; [
+      setuptools
+      wheel
+    ];
+    dependencies = with pythonPackages; [
+      numpy
+      scipy
+      pandas
+      scikit-learn
+      matplotlib
+    ];
+    doCheck = false;
+  };
+in
 {
   home.packages = with pkgs; [
-    (python313.withPackages (ps: with ps; [
+    (python.withPackages (ps: with ps; [
       ipython
       jupyter
+      ipykernel
+
       numpy
       pandas
       matplotlib
       seaborn
       geopandas
       scipy
-      sklearn-compat
+      scikit-learn
       imbalanced-learn
       nltk
       shap
@@ -21,16 +51,24 @@
       statsmodels
       graphviz
       flask
+
+      aif360
     ]))
   ];
+
   programs.neovim = {
     plugins = with pkgs.vimPlugins; [
-      (nvim-treesitter.withPlugins
-        (
-          plugins: with plugins; [
-            tree-sitter-python
-          ]
-        ))
+      (nvim-treesitter.withPlugins (plugins: with plugins; [
+        tree-sitter-python
+      ]))
     ];
   };
 }
+
+
+
+
+
+
+
+
