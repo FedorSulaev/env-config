18847938cb304bd030ce780ac5ce54b9c3b3b42c
Fedor Sulaev <1536446+FedorSulaev@users.noreply.github.com>
2026-04-08 19:21:00 +0300

    fix: Invalid Avante config

--- hosts/common/home-manager/modules/nvim-module/ai-module/avante-nvim.lua
+++ hosts/common/home-manager/modules/nvim-module/ai-module/avante-nvim.lua
@@ -1,10 +1,14 @@
 require("avante_lib").load()
 require("avante").setup({
     provider = "bedrock",
-    bedrock = {
-        model = "anthropic.claude-sonnet-4-20250514-v1:0",
-        max_tokens = 6553,
-        aws_profile = "cline-profile",
-        aws_region = "eu-west-3",
-    },
+    providers = {
+    	bedrock = {
+        	model = "anthropic.claude-sonnet-4-20250514-v1:0",
+        	aws_profile = "cline-profile",
+        	aws_region = "eu-west-3",
+		extra_request_body = {
+			max_tokens = 6553,
+		},
+    	},
+    }
 })
